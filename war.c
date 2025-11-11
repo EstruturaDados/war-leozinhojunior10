91fd5f8012bc7bf46f336fbfb4c624e7e98e9193
leozinhojunior10 <leozinho.junior11@gmail.com>
2025-11-11 08:52:58 -0300

    Improve input handling and add newline cleanup function
    Refactor input handling for territory data and add a function to remove newline characters from strings.

--- war.c
+++ war.c
@@ -2,51 +2,57 @@
 #include <string.h>
 
 typedef struct {
-    char nome[30];  
-    char cor[10];   
-    int tropas;   
+    char nome[30];
+    char cor[10];
+    int tropas;
 } Territorio;
-
 
 #define NUM_TERRITORIOS 5
 
+void limpar_quebra_linha(char *string) {
+    size_t len = strlen(string);
+    if (len > 0 && string[len - 1] == '\n') {
+        string[len - 1] = '\0';
+    }
+}
+
 int main() {
-  
-   
     Territorio listaTerritorios[NUM_TERRITORIOS];
+    int i;
     
-    int i; 
-
- 
     printf("--- Cadastro de %d Territórios ---\n\n", NUM_TERRITORIOS);
-
- 
+    
     for (i = 0; i < NUM_TERRITORIOS; i++) {
         printf("Território #%d:\n", i + 1);
 
-      
         printf("  Nome do Território (máx. 29 caracteres): ");
-        
-        scanf("%29s", listaTerritorios[i].nome); 
+        if (fgets(listaTerritorios[i].nome, sizeof(listaTerritorios[i].nome), stdin) == NULL) {
+            return 1;
+        }
+        limpar_quebra_linha(listaTerritorios[i].nome);
 
-      
         printf("  Cor do Exército (máx. 9 caracteres): ");
-        scanf("%9s", listaTerritorios[i].cor);
+        if (fgets(listaTerritorios[i].cor, sizeof(listaTerritorios[i].cor), stdin) == NULL) {
+            return 1;
+        }
+        limpar_quebra_linha(listaTerritorios[i].cor);
         
-       
         printf("  Quantidade de Tropas: ");
-      
-        scanf("%d", &listaTerritorios[i].tropas);
+        if (scanf("%d", &listaTerritorios[i].tropas) != 1) {
+            printf("Entrada inválida.\n");
+            return 1;
+        }
         
-        printf("\n"); 
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+        
+        printf("\n");
     }
 
-  
     printf("===================================\n");
     printf("=== Dados dos Territórios Cadastrados ===\n");
     printf("===================================\n\n");
 
-  
     for (i = 0; i < NUM_TERRITORIOS; i++) {
         printf("--- Território #%d ---\n", i + 1);
         printf("Nome:  **%s**\n", listaTerritorios[i].nome);
